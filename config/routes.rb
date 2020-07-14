447a4a0635d16fb708ca64eb7ede34f55792de0a
vagrant <vagrant@localhost.localdomain>
2020-07-14 12:19:35 +0000

    Update Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root :to => 'books#top'
-
   resources :books
-  get 'todobooks/:id' => 'todobooks#index', as: 'todobook'
+  root :to => 'books#new'
+
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
