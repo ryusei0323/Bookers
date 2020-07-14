b3f16bb8574ea60f7e392623b615aa3d436ec6d6
vagrant <vagrant@localhost.localdomain>
2020-07-14 12:23:41 +0000

    finish Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "successfully"
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
